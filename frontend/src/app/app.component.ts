c4234f64a935775e1940c5de84574aca44962b75
Arctic Fox <tyskanhik@mail.ru>
2025-07-08 17:50:26 +0300

    create log для проверки полученя данных

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardsService } from './core/services/cards.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'momentum';
+  private cardServices =  inject(CardsService)
+  cards = signal<any[]>([]) 
+
+  constructor() {
+    this.loadData()
+  }
+
+  loadData() {
+    this.cardServices.getCards().subscribe({
+      next: data => this.cards.set(data)
+    })
+  }
+
+  log() {
+    console.log(this.cards());
+  }
 }
