c79bd137097c80c475edd72d87b5563206832f03
Arctic Fox <tyskanhik@mail.ru>
2025-07-16 19:30:29 +0300

    create type uuid + validator?

---
+++ frontend/src/app/core/models/uuid.type.ts
@@ -0,0 +1,6 @@
+export type UUID = `${string}-${string}-${string}-${string}-${string}`;
+
+export function isUUID(value: string): value is UUID {
+  const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return regex.test(value);
+}
