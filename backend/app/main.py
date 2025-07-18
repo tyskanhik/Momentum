63d739ecb7794d59d25ef1f683754ae46e3d9bef
qusaebob <qusaebob@gmail.com>
2025-07-18 21:47:01 +0300

    crud for cards

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from .mock_cards import mock_cards, Card
+from uuid import UUID
 
 load_dotenv()  # Загружаем .env
 
@@ -18,9 +20,34 @@ app.add_middleware(
 def hello():
     return {"message": "Hello, FastAPI!"}
 
-@app.get("/items")
-def get_items():
-    return [
-        {"id": 1, "name": "Item 1"},
-        {"id": 2, "name": "Item 2"}
-    ]
+@app.get("/cards", response_model=list[Card])
+def get_cards():
+    return mock_cards
+
+@app.get("/cards/{card_id}", response_model=Card)
+def get_card(card_id: UUID):
+    for card in mock_cards:
+        if card.id == card_id:
+            return card
+    raise HTTPException(status_code=404, detail="Карточка не найдена")
+
+@app.post("/cards", response_model=Card)
+def create_card(card: Card):
+    mock_cards.append(card)
+    return card
+
+@app.put("/cards/{card_id}", response_model=Card)
+def update_card(card_id: UUID, card_data: Card):
+    for idx, card in enumerate(mock_cards):
+        if card.id == card_id:
+            mock_cards[idx] = card_data
+            return card_data
+    raise HTTPException(status_code=404, detail="Карточка не найдена")
+
+@app.delete("/cards/{card_id}")
+def delete_card(card_id: UUID):
+    for idx, card in enumerate(mock_cards):
+        if card.id == card_id:
+            del mock_cards[idx]
+            # return {"detail": "Карточка удалена"} не знаю нужно такое сообщение или нет
+    raise HTTPException(status_code=404, detail="Карточка не найдена")
